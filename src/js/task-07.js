b650538a2e0ab0a718a6f974c37d098fa438dae0
MBersPro <mbers.pro@gmail.com>
2021-07-21 21:06:59 +0300

    commit
    uihiu

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,13 +1,13 @@
-const fontSizeControlNode = document.querySelector('#font-size-control')
-const textNode = document.querySelector('#text')
+const fontSizeControlNode = document.querySelector("#font-size-control");
+const textNode = document.querySelector("#text");
 // fontSizeControlNode.setAttribute("min","1" )
 // fontSizeControlNode.setAttribute("max", "50")
 // fontSizeControlNode.setAttribute("step", "1")
-textNode.style.fontSize = '50px'
+textNode.style.fontSize = "50px";
 const fontSizeControl = () => {
-    const rangeValue = fontSizeControlNode.value
-    
-    textNode.style.fontSize = `${rangeValue}px`
-}
-fontSizeControlNode.addEventListener('input', fontSizeControl)
-console.log(fontSizeControlNode.value)
+  const rangeValue = fontSizeControlNode.value;
+
+  textNode.style.fontSize = `${rangeValue}px`;
+};
+fontSizeControlNode.addEventListener("input", fontSizeControl);
+console.log(fontSizeControlNode.value);
